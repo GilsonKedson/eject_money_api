5140762263c9a9486386be01abc513e77aa17d68
Gilson <gilsonkedson15@outlook.com>
2022-02-14 20:10:14 -0300

    fix: retirando doc momentaneamente

--- ejectmoney/settings.py
+++ ejectmoney/settings.py
@@ -26,9 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'drf_yasg',
     'rest_framework',
     'corsheaders',
-    'drf_yasg',
+    
     
     #my_apps
     'transacoes',
--- ejectmoney/urls.py
+++ ejectmoney/urls.py
@@ -1,27 +1,29 @@
 from django.urls import path, include
 from rest_framework import routers
 from transacoes.viewsets import TransactionViewSet
+'''
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-        openapi.Info(
-        title="EJECT MONEY API",
-        default_version='v1',
-        description="API PARA USO NA TRILHA REACT JS",
-        terms_of_service="#",
-        contact=openapi.Contact(email=""),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny,],
-    )
+   openapi.Info(
+      title="EJECT MONEY API",
+      default_version='v1',
+      description="API PARA TRILHA REACT JS",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+'''
 
 router = routers.DefaultRouter()
 router.register('transacoes', TransactionViewSet, basename='transacao')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    #path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
