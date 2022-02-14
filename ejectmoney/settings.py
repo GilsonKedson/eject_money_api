005b3a3abf35611fa8d8f5935fc0c571dc1ea316
Gilson <gilsonkedson15@outlook.com>
2022-02-14 17:53:13 -0300

    feat: add cors-headers PARCIAL

--- ejectmoney/settings.py
+++ ejectmoney/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     
     #my_apps
     'transacoes',
@@ -36,6 +37,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # CONFIGURAÇÃO CORS
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -90,6 +92,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+# CONFIGURAÇÃO DO CORS
+CORS_ALLOWED_ORIGINS  =  [ 
+    # ENDEREÇOS
+    "http://localhost:3000",
+    "http://localhost:5500",
 ]
 
 
